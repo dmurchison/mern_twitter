aa2cf8eb4ef9b2d22e90afdfeac10752559c544a
Duncan Murchison <duncancmurchison@gmail.com>
2022-07-05 01:43:40 -0400

    routes working

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-
+const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
@@ -12,13 +10,13 @@ mongoose
 .then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Welcome to MERN Twitter!"))
+app.get("/", (req, res) => {
+  res.send("Welcome to MERN Twitter!");
+});
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use("api/users", users);
-app.use("api/tweets", tweets);
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5001;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
